74d0bb359681b2afc19cadf38528ba436a185f0d
addstar <addstar@addstar.com.au>
2021-07-31 16:50:00 +1000

    MC 1.17.1/Java16 + Updated GP/WG/PS deps

--- src/main/java/au/com/mineauz/buildtools/protection/PSPlugin.java
+++ src/main/java/au/com/mineauz/buildtools/protection/PSPlugin.java
@@ -1,18 +1,9 @@
 package au.com.mineauz.buildtools.protection;
 
-import com.plotsquared.core.api.PlotAPI;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-
 public class PSPlugin implements ProtectionPlugin {
-	
-	private PlotAPI api;
-	
-	public PSPlugin(){
-		api = new PlotAPI();
-	}
-
 	@Override
 	public String getName() {
 		return "PlotSquared";
@@ -20,11 +11,16 @@ public class PSPlugin implements ProtectionPlugin {
 
 	@Override
 	public boolean canBuild(Player player, Location location) {
-		com.plotsquared.core.location.Location l = new com.plotsquared.core.location.Location(location.getWorld().getName(),
-				location.getBlockX(),location.getBlockY(),location.getBlockZ());
-		if(l.getPlotAbs() != null) {
+		com.plotsquared.core.location.Location l = com.plotsquared.core.location.Location.at(
+				location.getWorld().getName(),
+				location.getBlockX(),
+				location.getBlockY(),
+				location.getBlockZ()
+		);
+
+		if (l.getPlotAbs() != null) {
 			return l.getPlotAbs().isAdded(player.getUniqueId());
-		}else{
+		} else {
 			return false;
 		}
 	}
